5d6572aec4c8a2e85cbdd1b1dfc105c05fb86aad
trist <trist@LENOVOLOQ>
2025-08-15 08:41:26 -0600

    ACTUALIZACION DEL REPO CON LOS COMMITS ACTUALES.

--- src/guiutmedic/frmLogin.java
+++ src/guiutmedic/frmLogin.java
@@ -228,7 +228,7 @@ public class frmLogin extends javax.swing.JFrame {
 
         return accesoPermitido;
     }
-
+//IGNOREN ESTO 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
